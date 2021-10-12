b61437cb67a1d01b387707594055d1038163e54e
Fredric Agerling <f.agerling@hotmail.com>
2021-10-12 09:15:01 +0200

    Init

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Hej</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
